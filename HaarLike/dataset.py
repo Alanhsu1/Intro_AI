d1420a32cbdae9f949cf9c5ff9826ff883896462
Ray Huang <47706097+ExplorerRay@users.noreply.github.com>
2023-06-03 22:47:07 +0800

    Add files via upload

--- HaarLike/dataset.py
+++ HaarLike/dataset.py
@@ -1,32 +1,34 @@
 import os
-import cv2
 import tensorflow as tf
 import numpy as np
-from os import listdir
-from os.path import join, isfile
 
 def loadImages(dataPath):
-    # datapath = './CT'
-    dataset = []
-    dataPath1 = dataPath + "/COVID"
-    img_file = np.array([join(dataPath1, f) for f in listdir(dataPath1) if isfile(join(dataPath1, f))])
-    cnt = 0
-    
-    for file in img_file:
-        cnt += 1
-        if cnt == 2:
-            break
-        img = tf.keras.utils.load_img(file, target_size=(224, 224))
+    """
+    load all Images in the folder and transfer a list of tuples. The first 
+    element is the numpy array of shape (m, n) representing the image. 
+    The second element is its classification (1 or 0)
+      Parameters:
+        dataPath: The folder path.
+      Returns:
+        dataset: The list of tuples.
+    """
+    dataset = list()
+    for pt in os.listdir(dataPath):
+        for fn in os.listdir(dataPath+'/'+pt):
+            img = tf.keras.utils.load_img(dataPath+'/'+pt+'/'+fn, target_size=(24, 24))
 
-        img_tmp = tf.keras.utils.img_to_array(img)
-        img_tmp = tf.image.rgb_to_grayscale(img_tmp)
-        img_tmp = tf.cast(img_tmp, tf.int64)
-        # print(type(img_tmp))
-        img_tmp = np.expand_dims(img_tmp, axis=0)
-        print(img_tmp)
-        if cnt <= 100:
-            dataset.append((img_tmp, 1))
-        else:
-            dataset.append((img_tmp, 1))
+            img_tmp = tf.keras.utils.img_to_array(img)
+            img_tmp = tf.image.rgb_to_grayscale(img_tmp)
+            # float32 轉 int64
+            img_tmp = tf.cast(img_tmp, tf.int64)
+            # img_tmp shape is (24,24,1)
+            # type <class 'tensorflow.python.framework.ops.EagerTensor'>
 
+            if pt=='COVID': # label is 1 when it is a COVID
+                dataset.append((img_tmp, 1))
+            else:
+                dataset.append((img_tmp, 0))
     return dataset
+    # dataset is a list of tuples
+
+# dt = loadImages('CT')
